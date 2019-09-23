6192cfcebc45a910526c67c267cafb170505196c
Neetesh Bhardwaj <bhardwaj.neetesh@gmail.com>
2019-09-23 19:28:17 +0530

    Knock knockgit status

---
+++ src/main/java/com/trickyjava/greetsender/GreetsenderApplication.java
@@ -0,0 +1,13 @@
+package com.trickyjava.greetsender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetsenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetsenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trickyjava/greetsender/GreetsenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trickyjava.greetsender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetsenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
